433e6e026d61b43ab3fb0a196387ec554c2ae35c
Alvin Crespo <alvin.crespo@gmail.com>
2019-02-18 10:22:08 -0500

    Adds .next to .gitignore

--- .next/static/runtime/main.js
+++ .next/static/runtime/main.js
@@ -8384,7 +8384,7 @@ function () {
 
                       case 2:
                         return _context3.abrupt("return", new _promise.default(function (resolve) {
-                          ws = new WebSocket("".concat(wsProtocol, "://").concat(hostname, ":").concat(51242).concat("/"));
+                          ws = new WebSocket("".concat(wsProtocol, "://").concat(hostname, ":").concat(53703).concat("/"));
 
                           ws.onopen = function () {
                             return resolve();
