7fbb0918c4606d4cc5a473cbca16afcc4d9f0cbc
Alvin Crespo <alvin.crespo@gmail.com>
2019-02-24 10:22:27 -0500

    Implementing useEffects for data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { DefaultLayout } from '../layouts/Default';
 import { Inventory } from '../services/inventory';
 import { Personnel } from '../services/personnel';
 import { Manufacturers } from '../services/manufacturers';
 
-(async () => {
-  const inventory = await Inventory.fetch();
-  const personnel = await Personnel.fetch();
-  const manufacturers = await Manufacturers.fetch();
+const Index = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setLoading] = useState(true);
 
-  console.log({ inventory, personnel, manufacturers });
-})();
+  const fetchData = async () => {
+    setLoading(true);
 
-const Index = () => (
-  <DefaultLayout
-    render={({ Header }) => (
-      <>
-        <h1>Home Page</h1>
-        <Header />
-      </>
-    )}
-  />
-);
+    const inventory = await Inventory.fetch();
+    const personnel = await Personnel.fetch();
+    const manufacturers = await Manufacturers.fetch();
+
+    setData({ inventory, personnel, manufacturers });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(data);
+
+  return (
+    <DefaultLayout
+      render={({ Header }) => (
+        <>
+          <h1>Home Page</h1>
+          <Header />
+          {isLoading && <strong>Loading...</strong>}
+          {!isLoading &&
+            data.inventory.records.map(r => (
+              <div key={`product-${r.id}`}>
+                <strong>{r.fields['Product Name'][0]}</strong>
+              </div>
+            ))}
+        </>
+      )}
+    />
+  );
+};
 
 export default Index;
