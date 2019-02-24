31d9bf22924f8de5f053441fad10f20cd26dc095
Alvin Crespo <alvin.crespo@gmail.com>
2019-02-24 08:24:03 -0500

    fix(chore): adds prettier and eslint config (#12)

--- layouts/Default/layout.js
+++ layouts/Default/layout.js
@@ -1,3 +1,9 @@
-import { Header } from "../../components/Header";
+import React from 'react';
+import { Header } from '../../components/Header';
+import PropTypes from 'prop-types';
 
 export const DefaultLayout = ({ render }) => <div>{render({ Header })}</div>;
+
+DefaultLayout.propTypes = {
+  render: PropTypes.function
+};
