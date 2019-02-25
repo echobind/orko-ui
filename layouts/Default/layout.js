5a9a5b29eafd896631f3c17fbf63c59bf98b84da
Alvin Crespo <alvin.crespo@gmail.com>
2019-02-24 20:57:21 -0500

    Fix imports

--- layouts/Default/layout.js
+++ layouts/Default/layout.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Header } from '../../components/Header';
 import PropTypes from 'prop-types';
 import { Global } from '@emotion/core';
 import { Layout, globalStyles } from './styles';
+import { Header } from '../../components/Header';
 
 export const DefaultLayout = ({ render }) => (
   <Layout>
