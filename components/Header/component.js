0316414d9f53209934d9a8a9183b10cb6b80dad6
Alvin Crespo <alvin.crespo@gmail.com>
2019-02-24 21:18:35 -0500

    Adding nav styles

--- components/Header/component.js
+++ components/Header/component.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from '@emotion/styled';
 
-const linkStyle = {
-  marginRight: 15
-};
+const Navigation = styled.nav`
+  margin: 1rem 0;
+  padding: 1rem 0;
+  border: 1px solid #aeaeae;
+  border-left: none;
+  border-right: none;
+
+  a {
+    font-family: 'Roboto', sans-serif;
+    color: #aeaeae;
+    text-decoration: none;
+    margin: 0 1rem 0 0;
+    font-style: italic;
+    letter-spacing: 0.05rem;
+    font-size: 1.2rem;
+    position: relative;
+
+    &:hover {
+      font-weight: bold;
+    }
+
+    &:not(:last-child):after {
+      content: '';
+      position: absolute;
+      border-color: transparent transparent transparent #e2e1e1;
+      border-style: solid;
+      border-width: 2px;
+      transform: skewX(-15deg);
+      top: 0;
+      bottom: 0;
+      right: -12px;
+    }
+  }
+`;
 
 export const Header = () => (
   <div>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
+    <Navigation>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </Navigation>
   </div>
 );
